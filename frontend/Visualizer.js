e0b4bfc21147c50abef6fe4356808e119dc78fae
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-21 13:13:57 +0530

    feat(visualizer): song time elapsed UI

--- frontend/Visualizer.js
+++ frontend/Visualizer.js
@@ -1,3 +1,5 @@
+const elapsed = document.getElementById('elapsed');
+
 class Visualizer {
     /**
      * Creates a new visualizer
@@ -48,11 +50,12 @@ class Visualizer {
         this.bufferLength = this.analyzer.frequencyBinCount;
         this.data = new Uint8Array(this.bufferLength);
 
-        this.bar.width = (this.width / this.bufferLength) * 3;
+        this.bar.width = (this.width / this.bufferLength) * 2.5;
     }
 
     render() {
         requestAnimationFrame(this.render.bind(this));
+        elapsed.innerHTML = isNaN(this.audio.duration) ? ('00:00 / 00:00') : (`${this.convertTime(this.audio.currentTime)} / ${this.convertTime(this.audio.duration)}`);
         let dx = 0;
         this.analyzer.getByteFrequencyData(this.data);
 
@@ -74,5 +77,18 @@ class Visualizer {
             dx += this.bar.width + 1;
         }
 
+    }
+
+    convertTime(time) {
+        let mins = Math.floor(time / 60);
+        if (mins < 10)
+            mins = '0' + String(mins);
+
+        let secs = Math.floor(time % 60);
+        if (secs < 10)
+            secs = '0' + String(secs);
+
+
+        return `${mins}:${secs}`;
     }
 }
