9f94bd9be0e98b0973db16c28d3818585d28eb41
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-07 15:01:15 +0530

    feat(*): util methods

--- client/src/utils/Utils.ts
+++ client/src/utils/Utils.ts
@@ -3,6 +3,27 @@ class Utils {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
     }
+
+    static wait(time: number) {
+		return new Promise(resolve => {
+			setTimeout(resolve, time)
+		})
+	}
+
+    static async getState(): Promise<boolean> {
+		window.audio = document.createElement('audio');
+		document.getElementById('root').appendChild(window.audio);
+
+        window.audio.src = `${window.base}/audio?index=0`;
+        window.audio.crossOrigin = 'anonymous';
+
+		const promise = new Promise((resolve) => {
+			window.audio.onerror = () => resolve(false);
+			window.audio.addEventListener('loadeddata', () => resolve(true));
+		})
+
+		return (await (promise as unknown as Promise<boolean>));
+	}
 }
 
 export default Utils;
