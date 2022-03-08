250a9094662f72c3fb597c7f96e5848663db0bb2
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-08 23:07:22 +0530

    feat(*): unify input handlers

--- client/src/utils/Utils.ts
+++ client/src/utils/Utils.ts
@@ -1,3 +1,6 @@
+import Radio from "../components/Radio/Radio";
+import { App } from "../types/App";
+
 class Utils {
     static resize(canvas: HTMLCanvasElement) {
         canvas.width = window.innerWidth;
@@ -24,6 +27,33 @@ class Utils {
 
 		return (await (promise as unknown as Promise<boolean>));
 	}
+
+	static handleVolToggle() {
+		if (!(window.audio && (this as unknown as App).volumeBtn)) return;
+		if (window.muted) {
+			window.muted = false;
+			window.audio.volume = 1;
+			(this as unknown as App).volumeBtn.current.classList.remove('fa-volume-off');
+			(this as unknown as App).volumeBtn.current.classList.add('fa-volume-up');
+		} else {
+			window.muted = true;
+			window.audio.volume = 0;
+			(this as unknown as App).volumeBtn.current.classList.add('fa-volume-off');
+			(this as unknown as App).volumeBtn.current.classList.remove('fa-volume-up');
+		}
+
+	}
+
+	static handleKeys(e: KeyboardEvent) {
+        switch (e.code) {
+            case 'Space':
+                Utils.handleVolToggle.call(this);
+                break;
+            case 'ArrowRight': case 'ArrowLeft':
+                Radio.start();
+                break;
+        }
+    }
 }
 
 export default Utils;
